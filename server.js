f8a826253264a451278a3d1babcfecb30d8d8664
MrSuraj1 <surajyadavsy271@gmail.com>
2026-02-14 13:44:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-joined");
+  });
+
+  socket.on("offer", (data) => {
+    socket.to(data.roomId).emit("offer", data.offer);
+  });
+
+  socket.on("answer", (data) => {
+    socket.to(data.roomId).emit("answer", data.answer);
+  });
+
+  socket.on("ice-candidate", (data) => {
+    socket.to(data.roomId).emit("ice-candidate", data.candidate);
+  });
+
+});
+
+server.listen(5000, "0.0.0.0" ,() => console.log("Server running on 5000"));
